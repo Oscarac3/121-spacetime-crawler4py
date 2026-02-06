8193f6011453a382b115d1f35e47fd739537531a
Dat-Bois <eeshvij@gmail.com>
2026-02-05 16:28:36 -0800

    Refactor

--- crawler/scraper.py
+++ crawler/scraper.py
@@ -1,6 +1,6 @@
 import re
-from urllib.parse import urlparse, urljoin, urldefrag
 from bs4 import BeautifulSoup
+from urllib.parse import urlparse, urljoin, urldefrag
 
 
 def scraper(url, resp):
@@ -8,15 +8,17 @@ def scraper(url, resp):
     return [link for link in links if is_valid(link)]
 
 def extract_next_links(url, resp):
-    # Implementation required.
-    # url: the URL that was used to get the page
-    # resp.url: the actual url of the page
-    # resp.status: the status code returned by the server. 200 is OK, you got the page. Other numbers mean that there was some kind of problem.
-    # resp.error: when status is not 200, you can check the error here, if needed.
-    # resp.raw_response: this is where the page actually is. More specifically, the raw_response has two parts:
-    #         resp.raw_response.url: the url, again
-    #         resp.raw_response.content: the content of the page!
-    # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
+    '''
+    Implementation required.
+    url: the URL that was used to get the page
+    resp.url: the actual url of the page
+    resp.status: the status code returned by the server. 200 is OK, you got the page. Other numbers mean that there was some kind of problem.
+    resp.error: when status is not 200, you can check the error here, if needed.
+    resp.raw_response: this is where the page actually is. More specifically, the raw_response has two parts:
+            resp.raw_response.url: the url, again
+            resp.raw_response.content: the content of the page!
+    Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
+    '''
     if resp.status != 200 or resp.raw_response is None:
         return []
     try:
